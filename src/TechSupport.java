c048c09507aeb11810deb70381a9f955fd434088
Wang, Ruxuan <2745883W@student.gla.ac.uk>
2024-03-11 15:27:18 +0000

    Completed basic function of TechSupport

--- src/TechSupport.java
+++ src/TechSupport.java
@@ -1,41 +1,46 @@
 package src;
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TechSupport {
-
-    public public String getCurrentVersion() {
-        }
     
-    public void checkForUpdates() {
+    private String currentVersion;
+    private List<String> updateHistory = new ArrayList<>();
+
+    public TechSupport(String currentVersion) {
+        this.currentVersion = currentVersion;
     }
-        
-    public void applyUpdates(String newVersion) {
-    }   
+    
+    public void update(String newVersion) {
+        currentVersion = newVersion;
+        logUpdateActivity("Updated to " + newVersion);
+        updateHistory.add(newVersion);
+    }
     
         
     public void logUpdateActivity(String updateDetails) {
-            
+        System.out.println(updateDetails);
     }
     
-        
-    public void revertUpdate(String toVersion) {
-            
+    public void revertUpdate(String toVersion){
+        currentVersion = toVersion;
+        logUpdateActivity("Reverted to " + toVersion);
+        updateHistory.add(toVersion);
     }
 
     public List<String> getUpdateHistory() {
-            
-    }
-
-    public void fixBugs() {
-            
-    }
-
-    public void reportBugs() {
-            
+        return new ArrayList<>(updateHistory); 
     }
 
     public void notifyUsers() {
+        System.out.println("New version " + currentVersion + " is available. Please update your system.");
             
+    }
+
+    public void BugFix() {
+        System.out.println("Bug fixed in version " + currentVersion);
     }
 
 // This skeleton need to be concised and implemented
