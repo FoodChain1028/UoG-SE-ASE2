38ca9a24b04701bd3e3ea1235af4a7b637318d05
Wang, Ruxuan <2745883W@student.gla.ac.uk>
2024-03-11 14:28:11 +0000

    Created TechSupport.java and implemented a rough skeleton for that

---
+++ src/TechSupport.java
@@ -0,0 +1,43 @@
+package src;
+import java.util.List;
+
+public class TechSupport {
+
+    public public String getCurrentVersion() {
+        }
+    
+    public void checkForUpdates() {
+    }
+        
+    public void applyUpdates(String newVersion) {
+    }   
+    
+        
+    public void logUpdateActivity(String updateDetails) {
+            
+    }
+    
+        
+    public void revertUpdate(String toVersion) {
+            
+    }
+
+    public List<String> getUpdateHistory() {
+            
+    }
+
+    public void fixBugs() {
+            
+    }
+
+    public void reportBugs() {
+            
+    }
+
+    public void notifyUsers() {
+            
+    }
+
+// This skeleton need to be concised and implemented
+
+}
