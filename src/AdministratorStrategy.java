af1c1cd0ab62e542ba6666465c2d25f26e352b02
v19614880 <v19614880@gmail.com>
2024-03-12 21:00:56 +0000

    0

--- src/AdministratorStrategy.java
+++ src/AdministratorStrategy.java
@@ -1,4 +1,9 @@
 package src;
+
+import java.io.*;
+
+import static src.SchoolSystem.getUserInputString;
+
 /**
  * Strategy for Administrator actions.
  */
@@ -6,26 +11,54 @@ package src;
 public class AdministratorStrategy implements UserStrategy {
 
 	@Override
-	public void addRequirement() {
-		// TODO Auto-generated method stub
+	public void addRequirement() throws IOException {
+		String teachingPath = SystemConfiguration.getInstance().getteachingRequirementFilePath();
+		FileWriter writer = new FileWriter(new File(teachingPath).getAbsolutePath());
+		System.out.print("Enter teaching requirement: ");
+		String requirement = getUserInputString();
+		writer.write(requirement + "\n");
+		writer.close();
+		System.out.println("Administrator Requirement added successfully!");
 		
 	}
 
 	@Override
-	public void viewRequirement() {
-		// TODO Auto-generated method stub
+	public void viewRequirement() throws IOException {
+		String teachingPath = SystemConfiguration.getInstance().getteachingRequirementFilePath();
+		FileReader file = new FileReader(new File(teachingPath).getAbsolutePath());
+		BufferedReader reader = new BufferedReader(file);
+		String content;
+		while ((content = reader.readLine()) != null) {
+			System.out.println(content);
+		}
+		reader.close();
+		System.out.println("===The End of All Requirements===");
 		
 	}
 
 	@Override
-	public void addTrainingSession() {
-		// TODO Auto-generated method stub
+	public void addTrainingSession() throws IOException {
+		String teachingPath = SystemConfiguration.getInstance().getTrainingSessionFilePath();
+		FileWriter writer = new FileWriter (new File(teachingPath).getAbsolutePath(),true);
+		System.out.print("Enter training session: ");
+		String requirement = getUserInputString();
+		writer.write(requirement + "\n");
+		writer.close();
+		System.out.println("Requirement added successfully!");
 		
 	}
 
 	@Override
-	public void viewTrainingSession() {
-		// TODO Auto-generated method stub
+	public void viewTrainingSession() throws IOException {
+		String teachingPath = SystemConfiguration.getInstance().getTrainingSessionFilePath();
+		FileReader file = new FileReader(new File(teachingPath).getAbsolutePath());
+		BufferedReader reader = new BufferedReader(file);
+		String content;
+		while ((content = reader.readLine()) != null) {
+			System.out.println(content);
+		}
+		reader.close();
+		System.out.println("===The End of All Training sessions===");
 		
 	}
 
